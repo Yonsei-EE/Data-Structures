c3b05c1cd74cacb3e09ec79cd6e60358179903aa
Jiseung-lab <fhemfjsj23@gmail.com>
2018-03-30 15:39:09 +0900

    created functions

---
+++ Maze/list.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+List MakeEmpty(List L)
+{
+	if (L != NULL)
+		DeleteList(L);
+	L = (List)malloc(sizeof(struct Node));
+	if (L == NULL)
+		printf("Out of memory!");
+	L->Next = NULL;
+	return L;
+}
+
+int IsEmpty(List L)
+{
+	return L->Next == NULL;
+}
+
+int IsLast(Position P, List L)
+{
+	return P->Next == NULL;
+}
+
+Position Find(ElementType X, List L)
+{
+	Position P;
+
+	P = L->Next;
+	while (P != NULL && P->Element != X)
+		P = P->Next;
+
+	return P;
+}
+
+void Delete(ElementType X, List L)
+{
+	Position P, TmpCell;
+
+	P = FindPrevious(X, L);
+
+	if (!IsLast(P, L))
+	{
+		TmpCell = P->Next;
+		P->Next = TmpCell->Next;
+		free(TmpCell);
+	}
+}
+
+Position FindPrevious(ElementType X, List L)
+{
+	Position P;
+
+	P = L;
+	while (P->Next != NULL && P->Next->Element != X)
+		P = P->Next;
+
+	return P;
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+	Position TmpCell;
+
+	TmpCell = (List)malloc(sizeof(struct Node));
+	if (TmpCell == NULL)
+		printf("Out of space!!!");
+
+	TmpCell->Element = X;
+	TmpCell->Next = P->Next;
+	P->Next = TmpCell;
+}
+
+void DeleteList(List L)
+{
+	Position P, Tmp;
+
+	P = L->Next;
+	L->Next = NULL;
+	while (P != NULL)
+	{
+		Tmp = P->Next;
+		free(P);
+		P = Tmp;
+	}
+}
+
+void PrintList(List L)
+{
+	Position P;
+	P = L->Next;
+	while (P != NULL)
+	{
+		printf("%d ", P->Element);
+		P = P->Next;
+	}
+}
+
+Position Header(List L)
+{
+	return L;
+}
+
+Position First(List L)
+{
+	return L->Next;
+}
+
+Position Advance(Position P)
+{
+	return P->Next;
+}
+
+ElementType Retrieve(Position P)
+{
+	return P->Element;
+}
--- Maze/maze.c
+++ Maze/maze.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include "binheap.h"
 #include <stdlib.h>
+#include "binheap.h"
 
-PtrToCell NewNode(Priority F, PtrToCell Parent, int x, int y) {
+#define mazeWidth 15
+#define mazeHeight 15
+
+PtrToCell createNode(Priority F, PtrToCell Parent, int x, int y) {
 	PtrToCell newNode = malloc(sizeof(struct Cell));
 	newNode->F = F;
 	newNode->Parent = Parent;
@@ -10,16 +13,34 @@ PtrToCell NewNode(Priority F, PtrToCell Parent, int x, int y) {
 	newNode->y = y;
 }
 
-int main()
-{
+PtrToCell exploreNode(char maze[mazeWidth][mazeHeight], PriorityQueue fringe) {
+	int i, x, y;
+	PtrToCell currentNode, newNode;
+
+	currentNode = getMin(fringe);
+	x = currentNode->x;
+	y = currentNode->y;
+
+	// find non-wall adjacent nodes
+	if(x!=mazeWidth && maze[x+1][y]!='*')
+		
+
+
+	for(i=0; i<2; i++) {
+		newNode = createNode(3-i,NULL,0,i);
+		Insert(newNode, fringe);
+	}
+
+
+}
+
+void SetMaze(char maze[mazeWidth][mazeHeight]) {
 	int i, j;
 	long seed;
-	char maze[15][15];
-	PtrToCell Temp, newNode;
-	
+
 	// Initialize maze
-	for(i=0; i<15; i++) {
-		for(j=0; j<15; j++) {
+	for(i=0; i<mazeHeight; i++) {
+		for(j=0; j<mazeWidth; j++) {
 			maze[j][i] = '0';
 		}
 	}
@@ -28,31 +49,36 @@ int main()
 	printf("Type your student number: ");
 	scanf("%ld", &seed);
 	srand(seed);
-	for(i=0; i<15*15/2; i++) {
-		maze[rand()%15][rand()%15] = '*';
+	for(i=0; i<mazeWidth*mazeHeight/2; i++) {
+		maze[rand()%mazeWidth][rand()%mazeHeight] = '*';
 	}
 
 	// Set the start and goal
 	maze[0][0] = '0';
-	maze[14][14] = '0';
+	maze[mazeWidth-1][mazeHeight-1] = '0';
 
 	// Print maze
-	for(i=0; i<15; i++) {
-		for(j=0; j<15; j++) {
+	for(i=0; i<mazeHeight; i++) {
+		for(j=0; j<mazeWidth; j++) {
 			printf("%c ", maze[j][i]);
 		}
 		printf("\n");
 	}
+}
 
-	PriorityQueue Q = Initialize(1000);
+int main()
+{
+	int i, j;
+	char maze[mazeWidth][mazeHeight];
+	PtrToCell temp, startNode;
+	PriorityQueue fringe = Initialize(1000);
+	
+	SetMaze(maze);
 
-	for(i=0; i<2; i++) {
-		newNode = NewNode(3-i,NULL,0,i);
-		Insert(newNode, Q);
-	}
+	exploreNode(maze, fringe);
 
-	Temp = GetMin(Q);
-	printf("(%d,%d)\n", Temp->x, Temp->y);
+	temp = GetMin(fringe);
+	printf("(%d,%d)\n", temp->x, temp->y);
 
 	return 0;
 }
