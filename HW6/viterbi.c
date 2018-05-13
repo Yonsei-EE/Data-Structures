ae4c55d3771c6d7af706a7982d87159bb1fbfc54
Gram <fhemfjsj23@gmail.com>
2018-05-13 19:40:39 +0900

    HW6 viterbi

---
+++ HW6/viterbi.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+
+    fp = fopen("Noisefree_outputsignal.txt", 'r');
+    fscanf(fp, "%lf\t%lf", );
+
+    return 0;
+}
